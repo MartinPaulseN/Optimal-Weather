625c67c9e2ad768fdfca752bc614d8437a15887c
ITHS <martinpaulsen617@gmail.com>
2025-05-25 18:44:58 +0200

    Initial commit

---
+++ src/test/java/org/example/optimalweather/OptimalWeatherApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.optimalweather;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OptimalWeatherApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
