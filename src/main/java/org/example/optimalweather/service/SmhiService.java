717b98b3b586a0de670a29a13a565ee23e0f9c56
ITHS <martinpaulsen617@gmail.com>
2025-05-30 14:07:57 +0200

    Har gjort lite ändringar i SmhiService inom package service

--- src/main/java/org/example/optimalweather/service/SmhiService.java
+++ src/main/java/org/example/optimalweather/service/SmhiService.java
@@ -1,33 +1,32 @@
 package org.example.optimalweather.service;
 
 
-import com.fasterxml.jackson.databind.ObjectMapper;
 import org.example.optimalweather.smhi.model.SmhiForecast;
-import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.stereotype.Service;
-import org.springframework.web.client.RestTemplate;
+import org.springframework.web.reactive.function.client.WebClient;
+import reactor.core.publisher.Mono;
 
-import java.io.IOException;
 
 @Service
 public class SmhiService {
 
-    private final RestTemplate restTemplate;
-    private final ObjectMapper objectMapper;
+    private final WebClient webClient;
 
-    public SmhiService(RestTemplateBuilder builder) {
-        this.restTemplate = builder.build();
-        this.objectMapper = new ObjectMapper();
+    public SmhiService() {
+        this.webClient = WebClient.create();
     }
 
     public SmhiForecast getForecast(double lat, double lon) {
-        String url = String.format("https://opendata-download-metfcst.smhi.se/api/category/pmp3g/version/2/geotype/point/lon/18.0300/lat/59.3110/data.json", lon, lat);
+        String url = String.format(
+                "https://opendata-download-metfcst.smhi.se/api/category/pmp3g/version/2/geotype/point/lon/18.0300/lat/59.3110/data.json",
+                lon, lat
+        );
 
-        try {
-            String json = restTemplate.getForObject(url, String.class);
-            return objectMapper.readValue(json, SmhiForecast.class);
-        } catch (IOException e) {
-            throw new RuntimeException("Kunde inte läsa SMHI-data", e);
-        }
+        Mono<SmhiForecast> forecastMono = webClient.get()
+                .uri(url)
+                .retrieve()
+                .bodyToMono(SmhiForecast.class);
+
+        return forecastMono.block();
     }
 }
